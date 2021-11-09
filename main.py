2a6a1c38e070fe03a2a0f05d7732ac7997a749d5
alexandre9982 <93144729+alexandre9982@users.noreply.github.com>
2021-11-09 14:16:20 -0500

    Mise à jour main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -6,46 +6,41 @@ def on_forever():
     if distance < 5:
         basic.show_leds("""
             # # # # #
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
         """)
-    else:
-        if distance < 10: 
-            basic.show_leds("""
-                     # # # # #
-                     # # # # #
-                     . . . . .
-                     . . . . .
-                     . . . . .
-                    """)
-        else:
-                            if distance < 15:
-                                basic.show_leds("""
-                                         # # # # #
-                                         # # # # #
-                                         # # # # #
-                                         . . . . .
-                                         . . . . .
-                                        """)
-                            else:
-                                                if distance < 20:
-                                                    basic.show_leds("""
-                                                             # # # # #
-                                                             # # # # #
-                                                             # # # # #
-                                                             # # # # #
-                                                             . . . . .
-                                                            """)
-                                                else:
-                                                                    if distance < 25:
-                                                                        basic.show_leds("""
-                                                                                 # # # # #
-                                                                                 # # # # #
-                                                                                 # # # # #
-                                                                                 # # # # #
-                                                                                 # # # # #
-                                                                                """)
-  
+    elif distance < 10:
+        basic.show_leds("""
+            # # # # #
+                        # # # # #
+                        . . . . .
+                        . . . . .
+                        . . . . .
+        """)
+    elif distance < 15:
+        basic.show_leds("""
+            # # # # #
+                        # # # # #
+                        # # # # #
+                        . . . . .
+                        . . . . .
+        """)
+    elif distance < 20:
+        basic.show_leds("""
+            # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+                        . . . . .
+        """)
+    elif distance < 25:
+        basic.show_leds("""
+            # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+        """)
 basic.forever(on_forever)
